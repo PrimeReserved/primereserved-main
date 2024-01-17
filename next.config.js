97a60228dfa8861e2e176d1de452639f31162cae
Joshua-Chris <joshuadgreat04@gmail.com>
2024-01-17 07:55:13 +0100

    Edited next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["localhost"],
-    loader: "default", // Add this line to specify the default loader
-    path: "https://cdn.sanity.io", // Specify the base path for the remote images
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
   },
 };
 
