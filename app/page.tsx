1eb49ee17233eafa04ec3bc285046322ee340836
Joshua-Chris <joshuadgreat04@gmail.com>
2024-03-18 13:44:18 +0100

    Final Review with the UIUX Designers. Homepage #Jenny: Edited the ClientReviews Component.

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const testimonials = [
   {
     quote:
       "Working with PrimeReserved was a game changer for our business. Their team of skilled developers demonstrated exceptional professionalism, a keen understanding of our needs, and a commitment to delivering top-notch solutions. The communication was seamless, and the final product exceeded our expectations. PrimeReserved is our go-to choice for reliable and innovative software development.",
-    image: "/images/testimonials/david.JPG",
+    image: "/images/testimonials/davidAvatar.PNG",
     name: "Mr David Wills",
     position: "Software Engineer | SwiftPay",
     location: "Benin Republic",
@@ -31,7 +31,7 @@ const testimonials = [
   {
     quote:
       "We've had a fantastic experience working with PrimeReserved. Their attention to detail, commitment to quality, and prompt communication made the entire development process smooth and efficient. We highly recommend their services to anyone looking for top-notch software solutions.",
-    image: "/images/testimonials/liberty.jpeg",
+    image: "/images/testimonials/liberty.PNG",
     name: "Ms Sarah Johnson",
     position: "Product Manager | TechWise",
     location: "United States",
@@ -39,7 +39,7 @@ const testimonials = [
   {
     quote:
       "PrimeReserved exceeded our expectations with their exceptional software development services. From concept to delivery, their team demonstrated expertise, creativity, and professionalism. They truly understand our business needs and deliver innovative solutions that drive results.",
-    image: "/images/testimonials/liberty.jpeg",
+    image: "/images/testimonials/davidAvatar.PNG",
     name: "Mr Michael Brown",
     position: "CEO | DigitalSolutions",
     location: "Canada",
@@ -47,7 +47,7 @@ const testimonials = [
   {
     quote:
       "Working with PrimeReserved was a delightful experience. Their team is highly skilled, dedicated, and customer-oriented. They not only delivered a high-quality software solution but also provided valuable insights and recommendations throughout the process. We look forward to collaborating with them on future projects.",
-    image: "/images/testimonials/liberty.jpeg",
+    image: "/images/testimonials/liberty.PNG",
     name: "Ms Emily White",
     position: "CTO | CodeCrafters",
     location: "Australia",
@@ -55,7 +55,7 @@ const testimonials = [
   {
     quote:
       "PrimeReserved delivered exactly what we needed and more. Their professionalism, expertise, and commitment to customer satisfaction are commendable. We are extremely satisfied with the outcome of our project and would gladly recommend their services to others.",
-    image: "/images/testimonials/david.JPG",
+    image: "/images/testimonials/davidAvatar.PNG",
     name: "Mr John Smith",
     position: "Head of IT | TechHub",
     location: "United Kingdom",
--- components/ClientReviews/ClientReviews.tsx
+++ components/ClientReviews/ClientReviews.tsx
@@ -45,35 +45,39 @@ const ClientReviews: React.FC<ClientReviewsProps> = ({ testimonials }) => {
         }
       `}</style>
       <div className="mb-4 flex flex-row items-center justify-between md:p-8">
-        <div className="flex flex-col">
-          <p className="text-md mb-4 font-bold text-primary md:text-2xl">
-            CLIENT REVIEWS
-          </p>
-          <h2 className="text-lg font-bold text-white md:text-3xl">
-            See What Our Clients Say About Us!
-          </h2>
-        </div>
-        <div className="flex space-x-4">
-          <FaArrowLeft
-            onClick={scrollLeft}
-            className="text-md h-8 w-10 cursor-pointer rounded-lg bg-primary text-white"
-          />
-          <FaArrowRight
-            onClick={scrollRight}
-            className="text-md h-8 w-10 cursor-pointer rounded-lg bg-primary text-white"
-          />
+        <div className="flex w-full flex-col">
+          <p className="py-2 text-lg font-bold text-primary">CLIENT REVIEWS</p>
+          <div className="flex w-full items-center justify-between">
+            <h2 className="py-2 text-xl font-bold md:text-3xl">
+              See What Our Clients Say About Us!
+            </h2>
+            <div className="space-x-4 md:flex" hidden>
+              <span
+                className="cursor-pointer rounded-full bg-primary p-4"
+                onClick={scrollLeft}
+              >
+                <FaArrowLeft className="text-md h-5 w-5 cursor-pointer rounded-lg font-light text-white" />
+              </span>
+              <span
+                className="cursor-pointer rounded-full bg-primary p-4"
+                onClick={scrollRight}
+              >
+                <FaArrowRight className="text-md h-5 w-5 cursor-pointer rounded-lg font-light text-white" />
+              </span>
+            </div>
+          </div>
         </div>
       </div>
       <div className={`hide-scrollbar flex overflow-x-auto`}>
         {testimonials.map((testimonial, index) => (
           <div
             key={index}
-            className={`mx-4 flex flex-shrink-0 flex-col items-start space-y-4 rounded-lg border border-gray-200 p-4 ${
+            className={`mx-4 flex h-[500px] flex-shrink-0 flex-col items-start justify-evenly space-y-1 rounded-lg border border-gray-200 p-4 lg:h-[380px] ${
               index >= currentIndex && index < currentIndex + itemsPerPage
                 ? "md:flex"
                 : "hidden"
             }`}
-            style={{ minWidth: "300px", maxWidth: "calc(33.333% - 1rem)" }} // Adjusted widths
+            style={{ minWidth: "250px", maxWidth: "calc(33.333% - 1rem)" }} // Adjusted widths
           >
             <FaQuoteLeft className="text-3xl text-gray-600 dark:text-white" />
             <p className="text-sm text-black dark:text-customDarkTextColor">
@@ -85,7 +89,11 @@ const ClientReviews: React.FC<ClientReviewsProps> = ({ testimonials }) => {
                 alt="Client Image"
                 width={50}
                 height={50}
-                className="rounded-full"
+                style={{
+                  textAlign: "center",
+                  borderRadius: "50%",
+                  minHeight: "50px",
+                }}
               />
               <div className="flex flex-col">
                 <p className="text-sm font-bold">{testimonial.name}</p>
