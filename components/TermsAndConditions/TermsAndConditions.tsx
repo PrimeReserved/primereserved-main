cdf5749f74280d79342aec7c2a87dec1b5e75879
Joshua-Chris <joshuadgreat04@gmail.com>
2024-03-18 02:15:13 +0100

    Edited the TermsAndConditions Component

--- components/TermsAndConditions/TermsAndConditions.tsx
+++ components/TermsAndConditions/TermsAndConditions.tsx
@@ -8,51 +8,54 @@ const TermsAndConditions: React.FC = () => {
         Terms and Conditions Welcome to PrimeReserved! These terms and
         conditions outline the rules and regulations for the use of our website.
         By accessing this website, we assume you accept these terms and
-        conditions in full. Do not continue to use PrimeReserved's website if
-        you do not accept all of the terms and conditions stated on this page.
-        The following terminology applies to these Terms and Conditions, Privacy
-        Statement and Disclaimer Notice and any or all Agreements: "Client,"
-        "You," and "Your" refers to you, the person accessing this website and
-        accepting the Company's terms and conditions. "The Company,"
-        "Ourselves," "We," "Our," and "Us," refers to PrimeReserved. "Party,"
-        "Parties," or "Us," refers to both the Client and ourselves, or either
-        the Client or ourselves. All terms refer to the offer, acceptance, and
+        conditions in full. Do not continue to use PrimeReserved&apos;s website
+        if you do not accept all of the terms and conditions stated on this
+        page. The following terminology applies to these Terms and Conditions,
+        Privacy Statement and Disclaimer Notice and any or all Agreements:
+        &quot;Client,&quot; &quot;You,&quot; and &quot;Your&quot; refers to you,
+        the person accessing this website and accepting the Company&apos;s terms
+        and conditions. &quot;The Company,&quot; &quot;Ourselves,&quot;
+        &quot;We,&quot; &quot;Our,&quot; and &quot;Us,&quot; refers to
+        PrimeReserved. &quot;Party,&quot; &quot;Parties,&quot; or
+        &quot;Us,&quot; refers to both the Client and ourselves, or either the
+        Client or ourselves. All terms refer to the offer, acceptance, and
         consideration of payment necessary to undertake the process of our
         assistance to the Client in the most appropriate manner, whether by
         formal meetings of a fixed duration or any other means, for the express
-        purpose of meeting the Client's needs in respect of the provision of the
-        Company's stated services/products, in accordance with and subject to,
-        prevailing law of the USA. Any use of the above terminology or other
-        words in the singular, plural, capitalization and/or he/she or they, are
-        taken as interchangeable and therefore as referring to same. Cookies We
-        employ the use of cookies. By accessing PrimeReserved's website, you
-        agreed to use cookies in agreement with PrimeReserved's Privacy Policy.
-        Most interactive websites use cookies to let us retrieve the user's
-        details for each visit. Cookies are used by our website to enable the
-        functionality of certain areas to make it easier for people visiting our
-        website. Some of our affiliate/advertising partners may also use
-        cookies. License Unless otherwise stated, PrimeReserved and/or its
-        licensors own the intellectual property rights for all material on
-        PrimeReserved. All intellectual property rights are reserved. You may
-        access this from PrimeReserved for your own personal use subjected to
-        restrictions set in these terms and conditions. You must not: Republish
-        material from PrimeReserved Sell, rent or sub-license material from
-        PrimeReserved Reproduce, duplicate or copy material from PrimeReserved
-        Redistribute content from PrimeReserved User Comments This Agreement
-        shall begin on the date hereof. Certain parts of this website offer the
-        opportunity for users to post and exchange opinions, information,
-        material, and data 'Comments' in areas of the website. PrimeReserved
-        does not screen, edit, publish, or review Comments prior to their
-        appearance on the website and Comments do not reflect the views or
-        opinions of PrimeReserved, its agents, or affiliates. Comments reflect
-        the view and opinion of the person who posts such view or opinion. To
-        the extent permitted by applicable laws, PrimeReserved shall not be
-        responsible or liable for the Comments or for any loss, cost, liability,
-        damages, or expenses caused and/or suffered as a result of any use of
-        and/or posting of and/or appearance of the Comments on this website.
-        PrimeReserved reserves the right to monitor all Comments and to remove
-        any Comments which can be considered inappropriate, offensive, or causes
-        breach of these Terms and Conditions.
+        purpose of meeting the Client&apos;s needs in respect of the provision
+        of the Company&apos;s stated services/products, in accordance with and
+        subject to, prevailing law of the USA. Any use of the above terminology
+        or other words in the singular, plural, capitalization and/or he/she or
+        they, are taken as interchangeable and therefore as referring to same.
+        Cookies We employ the use of cookies. By accessing PrimeReserved&apos;s
+        website, you agreed to use cookies in agreement with
+        PrimeReserved&apos;s Privacy Policy. Most interactive websites use
+        cookies to let us retrieve the user&apos;s details for each visit.
+        Cookies are used by our website to enable the functionality of certain
+        areas to make it easier for people visiting our website. Some of our
+        affiliate/advertising partners may also use cookies. License Unless
+        otherwise stated, PrimeReserved and/or its licensors own the
+        intellectual property rights for all material on PrimeReserved. All
+        intellectual property rights are reserved. You may access this from
+        PrimeReserved for your own personal use subjected to restrictions set in
+        these terms and conditions. You must not: Republish material from
+        PrimeReserved Sell, rent or sub-license material from PrimeReserved
+        Reproduce, duplicate or copy material from PrimeReserved Redistribute
+        content from PrimeReserved User Comments This Agreement shall begin on
+        the date hereof. Certain parts of this website offer the opportunity for
+        users to post and exchange opinions, information, material, and data
+        &apos;Comments&apos; in areas of the website. PrimeReserved does not
+        screen, edit, publish, or review Comments prior to their appearance on
+        the website and Comments do not reflect the views or opinions of
+        PrimeReserved, its agents, or affiliates. Comments reflect the view and
+        opinion of the person who posts such view or opinion. To the extent
+        permitted by applicable laws, PrimeReserved shall not be responsible or
+        liable for the Comments or for any loss, cost, liability, damages, or
+        expenses caused and/or suffered as a result of any use of and/or posting
+        of and/or appearance of the Comments on this website. PrimeReserved
+        reserves the right to monitor all Comments and to remove any Comments
+        which can be considered inappropriate, offensive, or causes breach of
+        these Terms and Conditions.
       </p>
     </div>
   );
