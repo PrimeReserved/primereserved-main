a66cfe0e9da8d1850ddff5da4773e39e7e403b55
Joshua-Chris <joshuadgreat04@gmail.com>
2024-03-18 01:15:15 +0100

    Edited next.config.js: Changed 'images.domains' to 'images.remotePatterns'.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
         port: "",
       },
+      // Add more remote patterns if needed
     ],
   },
 };
